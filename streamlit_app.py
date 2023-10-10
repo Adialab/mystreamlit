7a9709e42852d7a4a8787262f561aaaf5095b291
Kenneth Tseng <fgtseng@gmail.com>
2023-10-10 13:18:04 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ df = pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
 })
-st.dataframe(df.style.highlight_max(axis=0))
+st.dataframe(df)
 
 chart_data = pd.DataFrame(
     np.random.randn(20, 3),
