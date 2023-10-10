647f2ccd02dd28438e8a3535b6d9bb2f925bc9ce
Kenneth Tseng <fgtseng@gmail.com>
2023-10-10 13:00:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,6 @@ st.line_chart(chart_data)
 map_data = pd.DataFrame(
     np.random.randn(100, 2) / [50, 50] + [23.9738835,120.9791046],
     columns=['lat', 'lon'],
-    zoom=3)
+    zoom=8)
 st.map(map_data)
 
